ebebedc99884dc133b7947cc650f05ddf78a232b
gichul <gichul0317@gmail.com>
2022-05-25 17:35:54 -0400

    fixed modal component

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import classes from './Checkout.module.css';
+
+function Checkout(props) {
+  const confirmHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postalcode">Postal Code</label>
+        <input type="text" id="postalcode" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" />
+      </div>
+      <button type="button" onClick={props.onCancel}>
+        Cancel
+      </button>
+      <button>Confirm</button>
+    </form>
+  );
+}
+
+export default Checkout;
